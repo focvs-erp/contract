1adf6d0c8d0e94f3f145b10eeb2276f8771031ef
Anderson Peruci <anderson.peruci@ax4b.com>
2021-06-09 09:36:53 -0300

    Implementando alterações de funcionalidade do Contrato de Medição

--- contract/models/contract.py
+++ contract/models/contract.py
@@ -646,3 +646,27 @@ class ContractContract(models.Model):
                 "terminate_date": False,
             }
         )
+
+
+    # AX4B - CPTM - CONTRATO MEDIÇÃO 
+    def action_receber_fatura(self):
+        # raise UserError('Função cancelada')
+        """ This function returns an action that display existing picking orders of given purchase order ids. When only one found, show the picking immediately.
+        """
+        result = self.env["ir.actions.actions"]._for_xml_id('stock.action_picking_tree_all')
+        # override the context to get rid of the default filtering on operation type
+        result['context'] = {'default_partner_id': self.partner_id.id, 'default_origin': self.name, 'default_picking_type_id': 1}
+        pick_ids = self.mapped('picking_ids')
+        # choose the view_mode accordingly
+        if not pick_ids or len(pick_ids) > 1:
+            result['domain'] = "[('id','in',%s)]" % (pick_ids.ids)
+        elif len(pick_ids) == 1:
+            res = self.env.ref('stock.view_picking_form', False)
+            form_view = [(res and res.id or False, 'form')]
+            if 'views' in result:
+                result['views'] = form_view + [(state,view) for state,view in result['views'] if view != 'form']
+            else:
+                result['views'] = form_view
+            result['res_id'] = pick_ids.id
+        return result
+    # AX4B - CPTM - CONTRATO MEDIÇÃO 
