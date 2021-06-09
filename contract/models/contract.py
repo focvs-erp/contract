05098028e51af5729bfd769014cc49b6ada12886
Marcelo Marcon <marcelo.marcon@ax4b.com>
2021-06-09 12:58:38 -0400

    Validação de campo para receber o produto

--- contract/models/contract.py
+++ contract/models/contract.py
@@ -736,6 +736,7 @@ class ContractContract(models.Model):
     def aplicar_em_um_produto(self, produto, produtos):
         # APLICAR UM FILTER NOS PRODUTOS DO SELF PRA OBTER O PRODUTO SOLICITADO
         for item in produtos:
+            ValidationError(f'{produto.id} {item.id}')
             if item.id == produto.id:
                 item.price_unit = self.calcular_novo_preco(reajuste_item, produto)
 
