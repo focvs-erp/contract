06ecfabfd316fc01501b153ea988b1066c0dfdfd
Douglas Lopes da Silva <douglas.silva@ax4b.com>
2021-06-11 19:57:26 -0300

    construção many2one reajuste

--- contract/models/contract.py
+++ contract/models/contract.py
@@ -131,7 +131,7 @@ class ContractContract(models.Model):
         string="Modifications",
     )
     #<!-- AX4B - CPTM - CONTRATO REAJUSTE DE PREÇO -->n
-    reajuste_preco = fields.Many2one(comodel_name="purchase.reajuste_preco", string="Reajuste de Preço")
+    reajuste_preco = fields.Many2one("ax4b_purchase.reajuste_preco", string="Reajuste de Preço")
     #<!-- AX4B - CPTM - CONTRATO REAJUSTE DE PREÇO -->
 
 
