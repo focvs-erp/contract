599bf3711400b6e38113f46b93bba2fcd1f4ec7d
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-06-30 12:01:44 -0300

    subindo campo porcentagem

--- contract/models/receber_fatura.py
+++ contract/models/receber_fatura.py
@@ -10,6 +10,8 @@ class ReceberFatura(models.Model):
     name = fields.Char(readonly=True)
     contract_id = fields.Many2one("contract.contract", invisible=True)
 
+    porcentagem = fields.Float(string="Porcentagem")
+
     partner_id = fields.Many2one("res.partner", string="Receber De")
     scheduled_date = fields.Date(string="Data Agendada", default=datetime.today())
     origin = fields.Char(related="contract_id.name", string="Documento de Origem")
