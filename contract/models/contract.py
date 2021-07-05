8b6b5e19ddd6c04dc67106a429f79689ac4fd65d
Guilherme Kimoto dos Santos <guilherme.santos@ax4b.com>
2021-07-05 09:57:56 -0300

    adicionando return ao método create

--- contract/models/contract.py
+++ contract/models/contract.py
@@ -857,6 +857,13 @@ class ContractContract(models.Model):
   
         self.env["contract.receber_fatura"].create(vals)
         self.env.cr.commit()
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'contract.receber_fatura',
+            'context': self.env.context,
+            'target': 'new'
+        }
         
 
     def _exist_receber_fatura_to_contrato_fornecedor(self):
