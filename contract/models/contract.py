ef32217cee64c6100ae2660b42d5a04852b77486
GabrielGomes125 <gabriel_gomesbatista@hotmail.com>
2021-07-05 10:13:59 -0300

    subindo views descomentadas

--- contract/models/contract.py
+++ contract/models/contract.py
@@ -895,7 +895,7 @@ class ContractContract(models.Model):
     # AX4B - CPTM - RATEIO FORNECEDOR 
 
     #AX4B - CPTM - RESERVA DE GARANTIA
-    # cod_reserva_garantia = fields.Selection([('10', '10%'),('20', '20%'),('30', '30%')],
-    #                                     string=" Reserva de Garantia")
-    # bt_reserva_garantia = fields.Boolean(default=False, string="Reserva de Garantia")
-    # cod_conta_contabil = fields.Many2one('account.account', 'Conta')
+    cod_reserva_garantia = fields.Selection([('10', '10%'),('20', '20%'),('30', '30%')],
+                                        string=" Reserva de Garantia")
+    bt_reserva_garantia = fields.Boolean(default=False, string="Reserva de Garantia")
+    cod_conta_contabil = fields.Many2one('account.account', 'Conta')
