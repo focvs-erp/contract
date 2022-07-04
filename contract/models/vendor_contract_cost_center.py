9897a5ffa1ac5a036e5953fd065984afd5b7d3a9
unknown <igor.oliveira@ax4b.com>
2022-07-04 12:33:46 -0300

    alteracao do campo cost_center

--- contract/models/vendor_contract_cost_center.py
+++ contract/models/vendor_contract_cost_center.py
@@ -5,8 +5,3 @@ class VendorContractCostCenter(models.Model):
     _inherit = 'contract.contract'
 
     cost_center = fields.Many2one('ax4b_accounting.cost_center')
-
-    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-        fields['cost_center'] = ", s.cost_center"
-        groupby += ', s.cost_center'
-        return super(VendorContractCostCenter, self)._query(with_clause, fields, groupby, from_clause)
