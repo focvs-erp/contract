c79b287cef5a2916d3b5e40991b3a093054ca319
unknown <igor.oliveira@ax4b.com>
2022-07-04 15:26:59 -0300

    alteracao do campo cost_center

--- contract/models/vendor_contract_cost_center.py
+++ contract/models/vendor_contract_cost_center.py
@@ -2,6 +2,6 @@ from odoo import fields, models
 
 
 class VendorContractCostCenter(models.Model):
-    # _inherit = 'contract.contract'
+    _inherit = 'contract.contract'
 
     cost_center = fields.Many2one('ax4b_accounting.cost_center')
