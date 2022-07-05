738faffdb3fad88a30fe1e36e06522053c96c39e
Guilherme Kimoto dos Santos <guilherme.santos@ax4b.com>
2022-07-05 17:42:18 -0300

    removendo atividade

--- contract/models/vendor_contract_cost_center.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class VendorContractCostCenter(models.Model):
-    _inherit = 'contract.contract'
-
-    # cost_center_id = fields.Many2one('ax4b_accounting.cost_center')
