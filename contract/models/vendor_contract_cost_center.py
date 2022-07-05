004263107b030eaff3a5eb8621b40b98c61f9f40
Guilherme Kimoto dos Santos <guilherme.santos@ax4b.com>
2022-07-05 17:18:56 -0300

    comentando campo relacional

--- contract/models/vendor_contract_cost_center.py
+++ contract/models/vendor_contract_cost_center.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class VendorContractCostCenter(models.Model):
     _inherit = 'contract.contract'
 
-    cost_center_id = fields.Many2one('ax4b_accounting.cost_center')
+    # cost_center_id = fields.Many2one('ax4b_accounting.cost_center')
