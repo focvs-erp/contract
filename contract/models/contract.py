b7b53c5353a8ece646a6d9456c2757321bbd78c1
LucasBergaminAX4B <lucas.santos@ax4b.com>
2022-07-26 15:25:38 -0300

    add timdelta import

--- contract/models/contract.py
+++ contract/models/contract.py
@@ -11,7 +11,7 @@ from odoo import api, fields, models
 from odoo.exceptions import UserError, ValidationError
 from odoo.tests import Form
 from odoo.tools.translate import _
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from dateutil.relativedelta import relativedelta
 
 
